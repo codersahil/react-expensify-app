10be95bb7bf9ef04918f867bbf135833b2c81fb4
root <root@localhost.localdomain>
2020-08-27 18:50:05 +0545

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
-const path = require('path')
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === "production";
+
+  console.log("env", env);
+  return {
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [new MiniCSSExtractPlugin()],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-}
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
+};
